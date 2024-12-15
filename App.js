c17a85497d15102c77eec190d2c341e6031a8581
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2024-12-15 04:24:04 -0600

    conditionally rendering links

--- App.js
+++ App.js
@@ -9,6 +9,51 @@ export default function App() {
   const contactMeHandler = () => {
     Linking.openURL("mailto:sandibek@gmail.com");
   };
+  const name = "Sam Ibraimov";
+  const occupation = "Frontend/Mobile Apps developer";
+  const links = {
+    github: "mygithublink",
+    linkedin: "mylinkedin",
+    instagram: "myinstagram",
+  };
+  const renderIcons = () => {
+    return (
+      <View style={styles.iconsContainer}>
+        {links.github && <FontAwesome5 name="github" size={24} color="black" />}
+        {links.linkedin && (
+          <FontAwesome5 name="linkedin" size={24} color="black" />
+        )}
+        {links.instagram && (
+          <FontAwesome6 name="square-instagram" size={24} color="black" />
+        )}
+      </View>
+    );
+  };
+  const aboutMe = () => (
+    <Text style={{ fontSize: 18, lineHeight: 35, padding: 10 }}>
+      I am a passionate software engineer with a dynamic career in both mobile
+      and web app development. My journey in technology began with a deep
+      interest in creating intuitive and accessible user interfaces, which has
+      grown into a professional expertise in crafting high-quality digital
+      experiences. With a strong foundation in modern JavaScript frameworks such
+      as React and React Native, I specialize in building scalable, responsive,
+      and visually appealing applications for both web and mobile platforms. In
+      the web domain, I have developed applications that prioritize performance
+      and accessibility, ensuring inclusivity and seamless interaction across
+      devices. My expertise includes TypeScript for type-safe coding, Material
+      UI for design consistency, and robust testing practices with tools like
+      Jest and Playwright. On the mobile side, I leverage React Native to
+      develop cross-platform apps that deliver native-like performance and user
+      experiences. My technical toolkit also extends to backend integration,
+      ensuring seamless communication between frontends and APIs. I thrive in
+      Agile environments, where collaboration and iterative development drive
+      success. Additionally, I am passionate about solving challenging problems,
+      from implementing advanced OCR technology for data extraction to designing
+      modular architectures for scalable projects. As a developer, I am
+      continuously learning and evolving, driven by a commitment to delivering
+      impactful software solutions that enhance users lives.
+    </Text>
+  );
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={["bottom"]}>
@@ -25,41 +70,11 @@ export default function App() {
               style={styles.profileImage}
               source={require("./assets/sandibek.png")}
             />
-            <Text style={styles.text}>Sam Ibraimov</Text>
-            <Text style={styles.paragraph}>Frontend/Mobile Developer</Text>
-            <View style={styles.iconsContainer}>
-              <FontAwesome5 name="github" size={24} color="black" />
-              <FontAwesome5 name="linkedin" size={24} color="black" />
-              <FontAwesome6 name="square-instagram" size={24} color="black" />
-            </View>
+            <Text style={styles.text}>{name}</Text>
+            <Text style={styles.paragraph}>{occupation}</Text>
+            {renderIcons()}
             <Button title="Contact me" onPress={contactMeHandler} />
-
-            <Text style={{ fontSize: 18, lineHeight: 35, padding: 10 }}>
-              I am a passionate software engineer with a dynamic career in both
-              mobile and web app development. My journey in technology began
-              with a deep interest in creating intuitive and accessible user
-              interfaces, which has grown into a professional expertise in
-              crafting high-quality digital experiences. With a strong
-              foundation in modern JavaScript frameworks such as React and React
-              Native, I specialize in building scalable, responsive, and
-              visually appealing applications for both web and mobile platforms.
-              In the web domain, I have developed applications that prioritize
-              performance and accessibility, ensuring inclusivity and seamless
-              interaction across devices. My expertise includes TypeScript for
-              type-safe coding, Material UI for design consistency, and robust
-              testing practices with tools like Jest and Playwright. On the
-              mobile side, I leverage React Native to develop cross-platform
-              apps that deliver native-like performance and user experiences. My
-              technical toolkit also extends to backend integration, ensuring
-              seamless communication between frontends and APIs. I thrive in
-              Agile environments, where collaboration and iterative development
-              drive success. Additionally, I’m passionate about solving
-              challenging problems, from implementing advanced OCR technology
-              for data extraction to designing modular architectures for
-              scalable projects. As a developer, I am continuously learning and
-              evolving, driven by a commitment to delivering impactful software
-              solutions that enhance users' lives.
-            </Text>
+            <>{aboutMe()}</>
           </View>
         </ScrollView>
       </SafeAreaView>
