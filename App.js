b541ffa1fe63467615e2c099f02fbe7a42098b56
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2024-12-15 01:54:26 -0600

    update assets and update home screen

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg",
+        }}
+        style={styles.imageContainer}
+      />
+      <Image
+        style={styles.profileImage}
+        source={require("./assets/vadim.png")}
+      />
+      <Text style={styles.text}>Sam Ibraimov</Text>
+      <Text style={styles.paragraph}>Frontend/Mobile Developer</Text>
     </View>
   );
 }
@@ -13,8 +23,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    width: "100%",
+    aspectRatio: 16 / 9,
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 150,
+    borderColor: "white",
+    borderWidth: 5,
+    marginTop: -75,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  paragraph: {
+    fontSize: 18,
   },
 });
